21f81fdf7d988d4d115e0f45089538d6488bfbe5
KirikV <anoprienkok@gmail.com>
2025-03-09 00:23:05 +0300

    data changes2

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,47 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
 
+fun suka(part1:String): String {
+    return part1
+        .map { char -> char + 1 }
+        .joinToString("")
+        .replace("5", "s")
+        .replace("4", "u")
+        .map { char -> char - 3 }
+        .joinToString("")
+        .replace("0", "o")
 }
+
+fun pzdc(part2:String): String {
+    return part2
+        .reversed()
+        .map { char -> char - 4 }
+        .joinToString("")
+        .replace("_", " ")
+}
+
+fun main(args: Array<String>) {
+    var s = "F2p)v\"y233{0->c}ttelciFc".toString()
+    var part1 = s.slice(0..<s.length / 2)
+    var part2 = s.slice(s.length / 2..<s.length)
+
+    println(suka(part1) + pzdc(part2))
+}
+
+
+//    var part1 = s.sliceArray(0..s.size / 2)
+//    var part2 = s.sliceArray(s.size / 2 + 1..<s.size)
+//    fun mute(part1: CharArray) {
+//        part1.map { char -> char + 3 }.joinToString(" ")
+//        return
+//    }
+//    mute(part1)
+//    println(part1.map { char -> char + 3 }.joinToString(" "))
+//    println(part1)
+
+
+
+
+
+
+
+
+
